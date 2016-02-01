39d9b341f9332af572c92ca85520b99c87b96d0e
James Pickles <picklesj85@gmail.com>
2016-02-01 19:31:36 +0000

    added part b

--- ErrorHandling.java
+++ ErrorHandling.java
@@ -1,20 +1,32 @@
 public class ErrorHandling {
 	public void launch() {
 		String str;
-		int result = 0;
-		int i = 0;
-		while (i < 10) {
+		int result = 0;	
+		int num = 0;
+		int count = 0;
+		boolean validInput = false;
+		while (!validInput) {
+			System.out.println("How many numbers would you like to enter?");
+			try {
+				num = Integer.parseInt(System.console().readLine());
+				validInput = true;
+			} catch (NumberFormatException ex) {
+				System.out.println("What you entered was not an integer!");
+			}
+		}				
+		while (num > 0) {
 			try {
 				System.out.println("Please enter a number: ");
 				str = System.console().readLine();
 				int tempInt = Integer.parseInt(str);
 				result += tempInt;
-				i++;
+				num--;
+				count++;
 			} catch (NumberFormatException ex) {
 				System.out.println("What you entered was not an integer!");
 			}
 		}
-		System.out.println("The average of you input is: " + result / 10);
+		System.out.println("The average of your input is: " + result / count);
 	}
 	
 	public static void main(String[] args) {
