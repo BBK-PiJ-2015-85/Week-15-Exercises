76143327d84c66dee7837900bd1693d52fce9a59
James Pickles <picklesj85@gmail.com>
2016-02-01 18:39:20 +0000

    added question 2

--- CodeFlow.java
+++ CodeFlow.java
@@ -2,6 +2,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class CodeFlow {
+	List<Integer> myList = new ArrayList<Integer>();
+	
 	public void launch(int userInput) {
 		List<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
@@ -30,8 +32,25 @@ public class CodeFlow {
 		}
 	}
 	
+	public void launch2() {
+		try {
+			myList.add(1);
+			myList.add(2);
+			myList.add(3);
+			myList.add(4);
+			myList.add(5);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} catch (NullPointerException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		CodeFlow cf = new CodeFlow();
-		cf.launch(6);
+		
+		//cf.launch(6);
+		
+		cf.launch2();
 	}
 }
